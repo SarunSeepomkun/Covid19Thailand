38dfcb10f5e03cf7a652d06ad16ccd4578abb5ff
SarunSeepomkun <63921535+SarunSeepomkun@users.noreply.github.com>
2020-12-09 11:59:47 +0700

    Update Covid19Summary.js

--- src/component/Covid19Summary.js
+++ src/component/Covid19Summary.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import Chart from 'react-chartjs-2';
 
 function Covid19Summary({props}) {
-    const province = props.Province;
+    // const province = props.Province;
 
     return (
         <div>
